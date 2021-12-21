3751193b17181cedaed7c572d756cc9dd61e2e97
hajar-co <boudali.hajar@gmail.com>
2021-12-21 12:21:40 +0100

    adding specialite model, dao

--- ForAcademy/src/main/java/utils/DBUtil.java
+++ ForAcademy/src/main/java/utils/DBUtil.java
@@ -6,48 +6,20 @@ import java.sql.SQLException;
 
 
 public class DBUtil {
+    private static final String HOST = "localhost";
+    private static final int PORT = 3308;
+    private static final String DB_NAME = "for_academy";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
     private static Connection connection;
-    private DBUtil() {}
-    public static synchronized Connection getConnection() throws DBException{
-        if(connection != null) {
-            return connection;
-        }
-        else {
-            try{
-                //user 'admin'@'localhost' password 'Admin@2022'
-                //db foracedemy
-                // set the DB url , user name, and password
-                String url = "jdbc:mysql://localhost:3308/blog";
-                String username = "root";
-                String password = "";
-                // get and return connection
-                connection = DriverManager.getConnection(url,username,password);
-                System.out.println("success");
-                return connection;
-            }
-            catch(SQLException e){
-                throw new DBException(e);
-                //System.out.println("not success");
-            }
-        }
 
-
-    }
-
-    public static void main(String[] args) throws DBException {
-        Connection cnx = DBUtil.getConnection();
-    }
-    public static synchronized void closeConnection() throws DBException{
-        if(connection != null) {
-            try {
-                connection.close();
-            }
-            catch(SQLException e){
-                throw new DBException(e);
-            }
-            finally {
-                connection = null;
-            }
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s:%d/%s", HOST,PORT,DB_NAME),USERNAME,PASSWORD);
+        }catch (SQLException se){
+            se.printStackTrace();
         }
+        return connection;
     }
 }
